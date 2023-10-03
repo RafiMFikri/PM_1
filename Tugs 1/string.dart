e343ff8cdb2257d53edf27fde9955d7f10a02cf0
RafiMFikri <145322273+RafiMFikri@users.noreply.github.com>
2023-10-03 17:24:36 +0700

    praktikum1

---
+++ Tugs 1/string.dart
@@ -0,0 +1,12 @@
+void main() {
+  // Membuat 3 variabel string
+  String string1 = "Rafi ";
+  String string2 = "Muhamad ";
+  String string3 = "Fikri";
+
+  // Menggabungkan ketiga string
+  String hasilGabungan = string1 + string2 + string3;
+
+  // Menampilkan hasilnya di layar
+  print(hasilGabungan);
+}
